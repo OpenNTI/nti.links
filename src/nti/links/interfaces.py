cbfbd6982448b50f9ce3a7899433f17a96788025
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-01 00:57:46 +0000

    make sure we get core interfaces
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.links/trunk@60534 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/links/interfaces.py
+++ src/nti/links/interfaces.py
@@ -9,66 +9,12 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from zope import interface
+import zope.deferredimport
+zope.deferredimport.initialize()
 
-from zope.mimetype.interfaces import mimeTypeConstraint
-
-from zope.schema import Iterable
-
-from nti.schema.field import ValidTextLine
-from nti.schema.field import ValidChoice as Choice
-from nti.schema.field import DecodingValidTextLine
-
-class ILink(interface.Interface):
-    """
-    A relationship between the containing entity and
-    some other entity.
-    """
-
-    rel = Choice(
-        title=u'The type of relationship',
-        values=('related', 'alternate', 'self', 'enclosure', 'edit', 'like',
-                'unlike', 'content'))
-
-    target = interface.Attribute(
-        """
-        The target of the relationship.
-
-        May be an actual object of some type or may be a string. If a string,
-        will be interpreted as an absolute or relative URI.
-        """)
-
-    elements = Iterable(
-        title="Additional path segments to put after the `target`",
-        description="""Each element must be a string and will be a new URL segment.
-
-        This is useful for things like view names or namespace traversals.""")
-
-    target_mime_type = DecodingValidTextLine(
-        title='Target Mime Type',
-        description="The mime type explicitly specified for the target object, if any",
-        constraint=mimeTypeConstraint,
-        required=False)
-
-    method = DecodingValidTextLine(
-        title='HTTP Method',
-        description="The HTTP method most suited for this link relation",
-        required=False)
-
-    title = ValidTextLine(
-        title="Human readable title",
-        required=False)
-
-class ILinkExternalHrefOnly(ILink):
-    """
-    A marker interface intended to be used when a link
-    object should be externalized as its 'href' value only and
-    not the wrapping object.
-    """
-
-class ILinked(interface.Interface):
-    """
-    Something that possess links to other objects.
-    """
-    links = Iterable(
-        title=u'Iterator over the ILinks this object contains.')
+zope.deferredimport.deprecatedFrom(
+    "Moved to nti.dataserver.core.interfaces",
+    "nti.dataserver.core.interfaces",
+    "ILink",
+    "ILinkExternalHrefOnly",
+    "ILinked")
--- src/nti/links/links.py
+++ src/nti/links/links.py
@@ -17,10 +17,10 @@ from functools import total_ordering
 from zope import interface
 from zope import component
 
+from nti.dataserver.core.interfaces import ILink
+
 from nti.externalization.persistence import NoPickle
 from nti.externalization.interfaces import IExternalObject
-
-from .interfaces import ILink
 
 @interface.implementer( ILink )
 @total_ordering
