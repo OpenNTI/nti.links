e91667d8226869afa557360d2a5bdf181e9e43b4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-08 02:30:19 +0000

    update classifiers
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.links/trunk@122889 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -34,8 +34,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     url="https://github.com/NextThought/nti.links",
     zip_safe=True,
